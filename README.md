f8a313de60428b04e69c8fc74622041c67041958
Thomas Mitchell <tmitchell@starkandwayne.com>
2015-11-23 14:42:27 -0500

    Added endpoints for parsing the uri string. Added support for trailing slashes on endpoints

--- README.md
+++ README.md
@@ -23,6 +23,28 @@ application's environment.
 Displays the connection uri for the Postgres database bound to the
 app. Successful if such a uri exists and the app can access it. Failure otherwise.
 
+###GET '/uri/username'  
+Displays the username found in the uri. Successful if uri exists and contains a username.  
+
+###GET 'uri/password'  
+Displays the password found in the uri. Successful if uri exists and contains a password.  
+
+###GET 'uri/location'  
+Displays the location found in the uri (i.e. the ip address or url). Successful
+if the uri exists and contains a location.  
+
+###GET 'uri/port'  
+Displays the connection port as found in the uri. Successful if the uri exists
+and contains a connection port.  
+
+###GET 'uri/dbname'  
+Displays the name of the database found in the uri. Successful if the uri exists
+and contains a database name.  
+
+###GET 'uri/params'  
+Displays the parameters of the connection string, as they are url-encoded. Successful
+if the uri exists and has parameters specified.
+
 ###POST '/exec'  
 Takes, as the body, an parameter named "sql" which is equal to the SQL query to be executed on the app's database. An example format from `curl` is as follows:
 
--- lib/app.rb
+++ lib/app.rb
@@ -24,6 +24,62 @@ def postgres_uri
   nil
 end
 
+# entries table (an entry not present in the uri will be nil)
+# :username string
+# :password string
+# :location string
+# :port     string
+# :dbname   string
+# :params   string
+def uri_entries
+	#uri format: postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
+	uri = postgres_uri
+	unless uri
+		return nil
+	end
+	if uri.index('postgresql://') == 0
+		uri = uri['postgresql://'.length...uri.length]
+	elsif uri.index('postgres://') == 0
+		uri = uri['postgres://'.length...uri.length]
+	else
+		return nil #if it doesn't start with that string, the uri is invalid.
+	end
+	entries = {}
+	login_cred_end = uri.index('@')
+	#if there's '@', then we need to parse login credentials
+	if login_cred_end
+		login_creds = uri[0...login_cred_end]
+		uri = uri[(login_cred_end + 1)...uri.length] #makes later code cleaner.
+		username_end = login_creds.index(':')
+		if username_end
+			entries[:username] = login_creds[0...username_end]
+			entries[:password] = login_creds[(username_end + 1)...login_creds.length]
+		else
+			entries[:username] = login_creds
+		end
+	end
+	params_start = uri.index('?')
+	if params_start
+		entries[:params] = uri[(params_start + 1)...uri.length]
+		uri = uri[0...params_start]
+	end
+	dbname_start = uri.index('/')
+	if dbname_start
+		entries[:dbname] = uri[(dbname_start + 1)...uri.length]
+		uri = uri[0...dbname_start]
+	end
+	port_start = uri.index(':')
+	if port_start
+		entries[:port] = uri[(port_start + 1)...uri.length]
+		uri = uri[0...port_start]
+	end
+	#what's left should be the url location
+	unless uri.empty?
+		entries[:location] = uri
+	end
+	return entries
+end
+
 #gets a fresh connection to the database
 def get_conn
   begin
@@ -34,8 +90,8 @@ def get_conn
 end
 
 #This should display the timestamp if a connection to the database was established.
-#The regex matches '/' or '/timestamp'
-get /\A\/(timestamp)?\z/ do
+#The regex matches '/' or '/timestamp' or '/timestamp/'
+get /\A\/(timestamp(\/)?)?\z/ do
   conn = get_conn
   if conn == nil
     body FAILURE_MESSAGE
@@ -49,19 +105,19 @@ get /\A\/(timestamp)?\z/ do
     rescue PG::Error
       status 409
       body FAILURE_MESSAGE
-    end
+		end
+    conn.close()
   end
-  conn.close()
 end
 
 #just an endpoint that should respond if this app is running.
-get '/ping' do
+get '/ping/?' do
   status 200
   body SUCCESS_MESSAGE
 end
 
 #displays the uri to access the postgres database
-get '/uri' do
+get /\A\/uri\/?\z/ do
   res = postgres_uri
   if res
     body "#{SUCCESS_MESSAGE}\n#{res}"
@@ -72,8 +128,71 @@ get '/uri' do
   end
 end
 
+# Most postgres service listings will probably have dbname, user, pass, etc
+# listed separately. However, I don't want that to be a requirement to use this
+# app, so I'll pull the necessary info direct from the uri.
 
-get '/services' do
+get '/uri/username/?' do
+	entries = uri_entries
+	if entries and entries[:username]
+		body "#{SUCCESS_MESSAGE}\n#{entries[:username]}"
+	else
+		body FAILURE_MESSAGE
+		status 409
+	end
+end
+
+get '/uri/password/?' do
+	entries = uri_entries
+	if entries and entries[:password]
+		body "#{SUCCESS_MESSAGE}\n#{entries[:password]}"
+	else
+		body FAILURE_MESSAGE
+		status 409
+	end
+end
+
+get '/uri/location/?' do
+	entries = uri_entries
+	if entries and entries[:location]
+		body "#{SUCCESS_MESSAGE}\n#{entries[:location]}"
+	else
+		body FAILURE_MESSAGE
+		status 409
+	end
+end
+
+get '/uri/port/?' do
+	entries = uri_entries
+	if entries and entries[:port]
+		body "#{SUCCESS_MESSAGE}\n#{entries[:port]}"
+	else
+		body FAILURE_MESSAGE
+		status 409
+	end
+end
+
+get '/uri/dbname/?' do
+	entries = uri_entries
+	if entries and entries[:dbname]
+		body "#{SUCCESS_MESSAGE}\n#{entries[:dbname]}"
+	else
+		body FAILURE_MESSAGE
+		status 409
+	end
+end
+
+get '/uri/params/?' do
+	entries = uri_entries
+	if entries and entries[:params]
+		body "#{SUCCESS_MESSAGE}\n#{entries[:params]}"
+	else
+		body FAILURE_MESSAGE
+		status 409
+	end
+end
+
+get '/services/?' do
   res = ENV['VCAP_SERVICES']
   if res
     body "#{SUCCESS_MESSAGE}\n#{res}"
@@ -85,7 +204,7 @@ get '/services' do
 end
 
 #execute an arbitrary, user-supplied query on the database
-post '/exec' do
+post '/exec/?' do
   conn = get_conn
   begin
     unless params['sql']
